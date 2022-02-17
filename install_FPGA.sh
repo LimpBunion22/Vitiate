10d91afd09143d3db1288893ce9651008983a98c
LimpBunion22 <r.daneelolivaw2@gmail.com>
2022-02-17 14:04:15 +0100

    refactoring gabriel lloron

--- install_FPGA.sh
+++ install_FPGA.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 cd ./utils/VIT-FPGA
-source install_VIT_FPGA.sh
+make
+cp ./include/netFPGA.h $HOME/workspace_development/include/
 
 cd ../../build
-# export LD_LIBRARY_PATH=$HOME/intel/oneapi/compiler/latest/linux/compiler/lib/intel64_lin:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=/home/hai/anaconda3/envs/Vitiate/lib:$LD_LIBRARY_PATH
 
-# export LD_LIBRARY_PATH=$HOME/intelFPGA_pro/18.1/hld/host/linux64/lib:$LD_LIBRARY_PATH
 make build
 make install
+
 cd ..
 conda activate Vitiate
-# for file in "$include"/*
-# do
-#     cp "$include"/"$file" $HOME/workspace_development/include/
-# done
