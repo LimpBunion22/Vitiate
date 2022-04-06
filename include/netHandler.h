cb12a2fb5b2e9d56a48126bb411d5ab717df4aec
gepet <gabichopo@gmail.com>
2022-04-06 13:49:26 +0200

    VIT 3.0.0 gradient formatted

--- include/netHandler.h
+++ include/netHandler.h
@@ -41,14 +41,13 @@ namespace net
         void net_create_random_from_vector(const std::string &net_key, int implementation, size_t n_ins, const std::vector<size_t> &n_p_l, const std::vector<int> activation_type);
         void net_create(const std::string &net_key, int implementation, bool random, const std::string &file, bool file_reload);
         std::vector<float> active_net_launch_forward(const std::vector<float> &inputs);
-        std::vector<float> active_net_launch_gradient(size_t iterations, size_t batch_size,
-                                                      float alpha, float alpha_decay, float lambda, float error_threshold, int norm, size_t dropout_interval, const std::string &file, bool file_reload);
-        std::vector<float> active_net_launch_gradient(const net::net_sets &sets, size_t iterations, size_t batch_size,
-                                                      float alpha, float alpha_decay, float lambda, float error_threshold, int norm, size_t dropout_interval);
+        void active_net_set_gradient_attribute(int attribute, float value);
+        std::vector<float> active_net_launch_gradient(size_t iterations, size_t batch_size, const std::string &file, bool file_reload);
+        std::vector<float> active_net_launch_gradient(const net::net_sets &sets, size_t iterations, size_t batch_size);
         void active_net_print_inner_vals();
         signed long active_net_get_gradient_performance();
         signed long active_net_get_forward_performance();
-        void active_net_write_net_to_file(const std::string &file);
+        void active_net_write_to_file(const std::string &file);
         void write_sets_to_file(const std::string &file, const net_sets &sets);
         void process_video(const std::string &video_name);
         std::vector<float> process_img_1000x1000(const std::vector<float> &image, bool dwz_10 = false);
